9216b0d2b6dcc7552005f678d3e0b2e54489cbab
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-15 13:10:17 +0300

    add search by countries, cities, properties ans test

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('owner')->group(function () {
+        Route::get('properties', [PropertyController::class, 'index']);
+        Route::post('properties', [PropertyController::class, 'store']);
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::get('bookings', [BookingController::class, 'index']);
+    });
 });
 
-Route::post('auth/register', RegisterController::class);
-
-Route::middleware('auth:sanctum')->group(function() {
-    Route::get('owner/properties', [PropertyController::class, 'index']);
-    Route::post('owner/properties', [PropertyController::class, 'store']);
-    Route::get('user/bookings', [BookingController::class, 'index']);
-});
+Route::get('search',
+    \App\Http\Controllers\Public\PropertySearchController::class);
